b4e15aebe69f392a589bcdc41e8c3a4b78cfd9dd
atedw823 <adamedwardsblackberry@gmail.com>
2025-05-27 13:39:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def list_sports():
         return jsonify({"error": response.text}), response.status_code
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
